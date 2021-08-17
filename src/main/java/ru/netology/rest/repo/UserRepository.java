5cac54855c0ff63ecda040f70cc6dc60dea6cbd5
Konstantin Shebalin <championacolironni@gmail.com>
2021-08-17 23:06:16 +0700

    Commit/t

--- src/main/java/ru/netology/rest/repo/UserRepository.java
+++ src/main/java/ru/netology/rest/repo/UserRepository.java
@@ -2,10 +2,29 @@ package ru.netology.rest.repo;
 
 import ru.netology.rest.authorities.Authorities;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserRepository {
+    private Authorities authorities;
+
+    Authorities write = Authorities.WRITE;
+    Authorities read = Authorities.READ;
+    Authorities delete = Authorities.DELETE;
+
+    /*Добрый вечер! А в чем, собственно, вопрос? Насчёт getUserAuthorities-это метод,
+     который должен вернуть список разрешений, проверив что пользователь существует.
+     Если не существует, то вернуть null. Там нет какого то скрытого смысла, просто вам нужно заполнить класс с этим методом тестовыми данными,
+     чтобы можно было проверить работоспособность вашего кода.*/
+
     public List<Authorities> getUserAuthorities(String user, String password) {
-       return ;
+        if (user != null & password != null) {
+            List<Authorities> authoritiesList = new ArrayList<>();
+            authoritiesList.add(write);
+            authoritiesList.add(delete);
+            authoritiesList.add(read);
+            return authoritiesList;
+        }
+        else return null;
     }
 }
